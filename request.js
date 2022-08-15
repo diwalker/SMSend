0ee3e8d5c30fff63d4add05e5e459b434c15f95a
diX <diwalker13@icloud.com>
2022-08-15 11:03:33 -0300

    Troca de dados

--- request.js
+++ request.js
@@ -2,12 +2,12 @@ var request = require('request');
 
 request({
   method: 'POST',
-  url: "https://api.nvoip.com.br/v2/sms?napikey=bEZiYlJPZE1pMlRkOVI2MmZsRE9VUmpqZUc4Z2NFNVc%3D",
+  url: "https://api.nvoip.com.br/v2/sms",
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer !GfB-YiOgeTS#CXab-Vjb9P76@VZXD-KC5W#3Si'
+    'Authorization': '' // Cole seu TOKEN aqui.
   },
-  body: "{  \"numberPhone\": \"(87)988370228\",  \"message\": \"TESTANDO MEU SCRIPT DE ENVIO DE SMS\",  \"flashSms\": false}"
+  body: "{  \"numberPhone\": \"(XX)XXXXXXXXX\",  \"message\": \"sua mensagem aqui\",  \"flashSms\": false}" // Trocar o Body, com os dados usados para envio do SMS.
 }, function (error, response, body) {
   console.log('Status:', response.statusCode);
   console.log('Headers:', JSON.stringify(response.headers));
